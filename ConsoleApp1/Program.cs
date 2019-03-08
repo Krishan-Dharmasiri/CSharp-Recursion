831d650d4337962642d8d50f5145a6f09c0020c8
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2019-03-08 16:36:45 +1100

    Created the console and added the class file to practise recursion

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print numbers from 1 to 10
+            Recursion.PrintNumbersOneToTen(1);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Print the sum from 1 to n
+            Console.WriteLine("--- Passed value is 5 so the Sum is 15 ---");
+            Console.WriteLine(Recursion.SumFromOneToAGivenNumber(5));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Factorial of the given value
+            Console.WriteLine("--- Passed values is 10 so the Factorial of 10 = 3,628,800 ---");
+            Console.WriteLine(Recursion.Factorial(10));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Recursion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Recursion
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n">The parameter that is used to stop the recursion</param>
+        public static void PrintNumbersOneToTen(int n)
+        {
+            if (n > 10)
+                return;
+
+            Console.WriteLine(n);
+            PrintNumbersOneToTen(n+1);
+
+        }
+
+        /// <summary>
+        /// Sum up the integers until the int value given
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int SumFromOneToAGivenNumber(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+                return n + SumFromOneToAGivenNumber(n - 1);
+        }
+
+        /// <summary>
+        /// Get the Factorial of the given value (int)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
